f7a69f729df6fc96e0705a52be15d2e23d3196aa
Ahmed12ibad <ahmedhamid777fast@gmail.com>
2022-06-28 03:42:32 -0700

    dise

---
+++ index.js
@@ -0,0 +1,82 @@
+// //task 1
+
+// // var user=prompt("enter the number","6")
+// //     var nam = Math.random()* user
+// //     var result = Math.ceil(nam)
+// // document.write("<h1>"+result+"<h1/>")
+
+// //
+
+// // var name=Math.round(.4)
+// // document.write(name)
+
+// // 
+
+// // var name=Math.floor(.9)
+// // document.write(name)
+
+// //  name=Math.ceil(.2)
+// // document.write(name)
+
+
+
+// //  task 2
+
+// var user1=prompt("player name","ahmed")
+// var toos1=prompt("choose 1 h/t")
+// var user2=prompt("player name","shayan")
+//  var toos2=prompt("choose 1 h/t")
+
+// var name=Math.random()* 1
+// var num= Math.round(name)
+
+// if(num === 1 && toos1 === "h" ){
+// alert(user1+" "+"head"+" "+"won")
+// }
+
+// else if(num === 1 && toos2 === "h"){
+//     alert(user2 +" "+"head"+" "+"won")
+// }
+
+// else if(num === 0 && toos1 === "t"){
+//     alert(user1+" "+"tial"+" "+"won")
+// }
+
+// else if(num === 0 && toos2 === "t"){
+//     alert(user2+" "+"tial"+" "+"won")
+// }
+
+
+
+
+// task 3
+
+// var number=[0,1,2,3,4,5,7,8,9]
+
+// var A =['A','B','C','D','F','G','D','E','G','G','G','G','H','F','D']
+
+// var a=['a','b','g','d','g','h','z','g','c','j','e','e','t','g','d','z','q','d'] 
+
+// var cha=["#","/","&","@","*"]
+
+// var user1=number[Math.floor(Math.random()*number.length)]
+
+// var user2=A[Math.floor(Math.random()*A.length)]
+
+
+// var user3=a[Math.floor(Math.random()*a.length)]
+
+
+// var user4=cha[Math.floor(Math.random()*cha.length)]
+
+// var result=user1+user2+user3+user4
+// document.write(result)
+
+// document.write("<br>")
+
+//
+
+// var aplha=['a','b','c','d']
+
+// var name=aplha[Math.floor(Math.random()*aplha.length)]
+// document.write(name)
